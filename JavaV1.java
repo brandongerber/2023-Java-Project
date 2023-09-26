eb89cc13b9b462480471c0f6ff95369e1ee5c8e0
Brandon Gerber <90160923+brandongerber@users.noreply.github.com>
2023-09-26 13:25:51 -0400

    JAVAV1 Revision 2
    I switched around some things, tried to get rid of all if statements, methods, and loops. Added questions but i did not add where it keeps score. I was just going off on how the template looked.

--- JavaV1.java
+++ JavaV1.java
@@ -3,40 +3,78 @@ import java.util.Scanner;
 public class JavaV1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String name;
 
-        // Prompt the user for their name
-        System.out.print("Welcome to Who Wants to Be a Java Programmer? Press Enter to begin!");
-        String playerName = scanner.nextLine();
-        System.out.println("Please enter your name here: ");
-        String name = scanner.nextLine();
-        System.out .println("Hello " +name);
-        // Display the main menu
-        System.out.println("Main Menu:");
+        System.out.println("Welcome to Who Wants to Be a Java Programmer?");
+        System.out.print("Please enter your name: ");
+        name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("\nMain Menu:");
         System.out.println("1) See Rules");
-        System.out.println("2) Play Game");
+        System.out.println("2) Start Test");
         System.out.println("3) Exit");
-
-        // Prompt the user to choose an option
         System.out.print("Choose an option (1/2/3): ");
         int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
 
-        // Handle user's choice
         switch (choice) {
             case 1:
-                // Display rules
-                System.out.println("1. You will be tested on a set of 10 questions relating to Java.");
-                System.out.println("2. After each question, you will receive points for each correct answer.");
-                System.out.println("3. At the end of the test, you will receive a final score.");
+                System.out.println("\nRules of the test:");
+                System.out.println("1. You will be tested on a set of 3 multiple-choice questions relating to Java.");
+                System.out.println("2. These questions relate to chapters 1 and 2");
+                System.out.println("3. Have fun!");
+                System.out.println("\nPress Enter to start the test...");
+                scanner.nextLine(); // Wait for Enter key
+                startTest(scanner, name);
                 break;
             case 2:
-                // Exit the game
-                System.out.println("Thank you for playing. Goodbye, " + playerName + "!");
+                startTest(scanner, name);
+                break;
+            case 3:
+                System.out.println("Thank you for playing. Goodbye, " + name + "!");
                 break;
             default:
                 System.out.println("Invalid choice. Please choose a valid option (1/2/3).");
         }
 
-        // Close the scanner
         scanner.close();
+    }
+
+    private static void startTest(Scanner scanner, String name) {
+        int score = 0;
+
+        // Question 1
+        System.out.println("\nQuestion 1: What does JVM stand for?");
+        System.out.println("A)Java Virtual Machine");
+        System.out.println("B)Java Very Main");
+        System.out.println("C)Java Virtual Method");
+        System.out.println("D)Java Viable Memory");
+        System.out.print("Your answer (A/B/C/D): ");
+        char userAnswer1 = scanner.next().toUpperCase().charAt(0);
+        scanner.nextLine();
+
+        // Question 2
+        System.out.println("\nQuestion 2:What is the java source file extension?");
+        System.out.println("A).javaexe");
+        System.out.println("B).javas");
+        System.out.println("C).java");
+        System.out.println("D).exe");
+        System.out.print("Your answer (A/B/C/D): ");
+        char userAnswer2 = scanner.next().toUpperCase().charAt(0);
+        scanner.nextLine();
+        
+        // Question 3
+        System.out.println("\nQuestion 3: What are comments used for?");
+        System.out.println("A)To tell the compiler what it needs to do");
+        System.out.println("B)To close a line of code");
+        System.out.println("C)To keep reminders and notes");
+        System.out.println("D)To define the scope of the class");
+        System.out.print("Your answer (A/B/C/D): ");
+        char userAnswer3 = scanner.next().toUpperCase().charAt(0);
+        scanner.nextLine();
+
+        System.out.println("\nThank you for playing, " + name + "!");
+     
     }
 }
