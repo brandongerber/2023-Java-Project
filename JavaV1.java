0880144d0698e3ff83d98b69bf4ef8e204bb2279
Brandon Gerber <90160923+brandongerber@users.noreply.github.com>
2023-10-20 12:02:26 -0400

    Update JavaV1.java

--- JavaV1.java
+++ JavaV1.java
@@ -1,25 +1,4 @@
-/*
- Who Wants To Be A Programmer Algorithim
- Declared Vaiables score, userAnswer1-5, userAnswer22
- Variable types: int, char
- 1. Display Prompt
- 2. Enter Name
- 3. Greeting
- 4. Menu
- - Display
- - Start Game
- - Exit
- * (Rules Display)
- - Leads back to Start of game
- * (Start Game)
- - Question 1
- - Question 2
- - Question 3
- - Question 4
- - Question 5
- * (Exit)
- - Display Exit Prompt
- */
+// we just need to add the pseudocode
 import javax.swing.JOptionPane;
 
 public class JavaV2 {
@@ -31,35 +10,29 @@ public class JavaV2 {
 
         JOptionPane.showMessageDialog(null, "Hello, " + name + "!", "Introduction", JOptionPane.PLAIN_MESSAGE);
 
-        while (true) {
-            Object[] options = {"See Rules", "Start Test", "Exit"};
-            int choice = JOptionPane.showOptionDialog(null, "Main Menu:", "Menu",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
-                    null, options, options[2]);
-           // added option to choice between options with an invalid choice if it was any number but 1/2/3
-            switch (choice) {
-                case 0:
-                    JOptionPane.showMessageDialog(null, "Rules of the test:\n" +
-                            "1. You will be tested on a set of 5 multiple-choice questions relating to Java.\n" +
-                            "2. These questions relate to chapters one, two, and three.\n" +
-                            "3. Have fun!", "Rules", JOptionPane.INFORMATION_MESSAGE);
-                    startTest(name);
-                    break;
-                case 1:
-                    startTest(name);
-                    break;
-                case 2:
-                    JOptionPane.showMessageDialog(null, "Thank you for playing. Goodbye, " + name + "!",
-                            "Goodbye", JOptionPane.PLAIN_MESSAGE);
-                    System.exit(0);
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Invalid choice. Please choose a valid option (1/2/3).",
-                            "Error", JOptionPane.ERROR_MESSAGE);
-            }
+        Object[] options = {"See Rules", "Start Test", "Exit"};
+        int choice = JOptionPane.showOptionDialog(null, "Main Menu:", "Menu",
+                JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+                null, options, options[2]);
+
+        if (choice == 0) {
+            JOptionPane.showMessageDialog(null, "Rules of the test:\n" +
+                    "1. You will be tested on a set of 5 multiple-choice questions relating to Java.\n" +
+                    "2. These questions relate to chapters one, two, and three.\n" +
+                    "3. Have fun!", "Rules", JOptionPane.INFORMATION_MESSAGE);
+            startTest(name);
+        } else if (choice == 1) {
+            startTest(name);
+        } else if (choice == 2) {
+            JOptionPane.showMessageDialog(null, "Thank you for playing. Goodbye, " + name + "!",
+                    "Goodbye", JOptionPane.PLAIN_MESSAGE);
+            System.exit(0);
+        } else {
+            JOptionPane.showMessageDialog(null, "Invalid choice. Please choose a valid option (1/2/3).",
+                    "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    // added a startest method 
+
     private static void startTest(String name) {
         score = 0;
 
@@ -70,8 +43,8 @@ public class JavaV2 {
                 "C) Java Virtual Method\n" +
                 "D) Java Viable Memory\n" +
                 "Your answer (A/B/C/D): ").toUpperCase();
-        // if user answers correctly they gain one point and it also shows their current score
-        if (answer1.equals("A")) {
+
+        if (answer1.equalsIgnoreCase("A")) {
             score += 1; // Assigned a point value for Question 1
             JOptionPane.showMessageDialog(null, "Correct! You gained 1 point. Your current score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         } else {
@@ -86,7 +59,7 @@ public class JavaV2 {
                 "D) .exe\n" +
                 "Your answer (A/B/C/D): ").toUpperCase();
 
-        if (answer2.equals("C")) {
+        if (answer2.equalsIgnoreCase("C")) {
             score += 1; // Assigned a point value for Question 2
             JOptionPane.showMessageDialog(null, "Correct! You gained 1 point. Your current score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         } else {
@@ -101,14 +74,14 @@ public class JavaV2 {
                 "D) To define the scope of the class\n" +
                 "Your answer (A/B/C/D): ").toUpperCase();
 
-        if (answer3.equals("C")) {
+        if (answer3.equalsIgnoreCase("C")) {
             score += 1; // Assigned a point value for Question 3
             JOptionPane.showMessageDialog(null, "Correct! You gained 1 point. Your current score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Incorrect. The correct answer is C. Your current score: " + score, "Result", JOptionPane.ERROR_MESSAGE);
         }
 
-        // added two more questions 
+        // Question 4
         String answer4 = JOptionPane.showInputDialog("Question 4: The else statement means what? \n" +
                 "A) It means the boolean statement is true\n" +
                 "B) That your code has an error\n" +
@@ -116,7 +89,7 @@ public class JavaV2 {
                 "D) You have more options to choose \n" +
                 "Your answer (A/B/C/D): ").toUpperCase();
 
-        if (answer4.equals("C")) {
+        if (answer4.equalsIgnoreCase("C")) {	
             score += 1; // Assigned a point value for Question 4
             JOptionPane.showMessageDialog(null, "Correct! You gained 1 point. Your current score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         } else {
@@ -131,12 +104,13 @@ public class JavaV2 {
                 "D) All of the above\n" +
                 "Your answer (A/B/C/D): ").toUpperCase();
 
-        if (answer5.equals("D")) {
+        if (answer5.equalsIgnoreCase("D")) {
             score += 1; // Assigned a point value for Question 5
             JOptionPane.showMessageDialog(null, "Correct! You gained 1 point. Your current score: " + score, "Result", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, "Incorrect. The correct answer is D. Your current score: " + score, "Result", JOptionPane.ERROR_MESSAGE);
         }
+
         // added a total score at the end of the test
         JOptionPane.showMessageDialog(null, "Thank you for playing, " + name + "!\nYour total score is: " + score,
                 "Test Results", JOptionPane.INFORMATION_MESSAGE);
